bf735baeae1452595c9168694117b4d3814f5dab
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-09-01 12:28:21 +0300

    Add files via upload

--- FinalExam/Program.cs
+++ FinalExam/Program.cs
@@ -1,88 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Problem1
+namespace problem2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string username = Console.ReadLine();
-
+            List<string> coockis = Console.ReadLine()
+                .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
             string input = Console.ReadLine();
 
-            while (input != "Sign up")
+            while (input != "No More Money")
             {
-                string[] comand = input.Split();
-                if (comand[0] == "Case")
+                string[] comand = input.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+                if (comand[0]== "OutOfStock")
                 {
-                    if (comand[1] == "lower")
+                    if (coockis.Contains(comand[1]))
                     {
-                        username = username.ToLower();
-                        Console.WriteLine(username);
-                    }
-                    else if (comand[1] == "upper")
-                    {
-                        username = username.ToUpper();
-                        Console.WriteLine(username);
+                        coockis.RemoveAll(x => x == comand[1]);
+                        
+                      
+
+                        //for (int i = 0; i < coockis.Count; i++)
+                        //{
+                        //    for (int j = i; j < coockis.Count-1; j++)
+                        //    {
+                        //        int inx = coockis.IndexOf(comand[1]);
+                        //        coockis.RemoveAt(inx);
+                        //    }
+                        //} 
                     }
                 }
-                else if (comand[0] == "Reverse")
+                else if (comand[0]== "Required")
                 {
-                    string emp = String.Empty;
-                    int strarind = int.Parse(comand[1]);
-                    int Endind = int.Parse(comand[2]);
-                    if (strarind > 0 && Endind < username.Length)
+                    int ind = int.Parse(comand[2]);
+                    if (ind < coockis.Count && ind >=0)
                     {
-                        emp = username.Substring(strarind, Endind-strarind+1);
-                        string revers = string.Empty;
-                        for (int i = emp.Length - 1; i >= 0; i--)
-                        {
-                            revers += emp[i];
-                        }
-                        Console.WriteLine(revers);
-                    }
-                    else
-                    {
-                        input = Console.ReadLine();
-                        continue;
+                        coockis.Insert(ind, comand[1]);
+                        coockis.RemoveAt(ind+1);
                     }
                 }
-                else if (comand[0] == "Cut")
+                else if (comand[0]== "Last")
                 {
-                    string task = comand[1];
-                    if (username.Contains(task))
-                    {
-                        int indexstart = username.IndexOf(task[0]);
-                        int endindex = username.IndexOf(task[task.Length - 1]);
-                        username = username.Remove(indexstart, endindex - indexstart + 1);
-                        Console.WriteLine(username);
-                    }
-                    else
-                    {
-                        Console.WriteLine($"The word peter doesn't contain {task}.");
-                    }
-                }
-                else if (comand[0] == "Replace")
-                {
-                    string letter = comand[1];
-                    string sinbol = "*";
-                    username = username.Replace(letter, sinbol);
-                    Console.WriteLine(username);
-                }
-                else if (comand[0] == "Check")
-                {
-                    char simbol = char.Parse(comand[1]);
-                    if (username.Contains(simbol))
-                    {
-                        Console.WriteLine("Valid");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Your username must contain {simbol}.");
-                    }
+                    coockis.Add(comand[1]);
                 }
                 input = Console.ReadLine();
             }
+            Console.WriteLine(string.Join(" ",coockis));
         }
     }
 }
