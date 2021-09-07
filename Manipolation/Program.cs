a3226c7d6bd39abc17776140e19956a105d408fe
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-09-07 22:50:23 +0300

    Add files via upload

---
+++ Manipolation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace _05._Multiplication_Sign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int three = int.Parse(Console.ReadLine());
+
+            if (first < 0 || second < 0 || three <0)
+            {
+                Console.WriteLine("negative");
+            }
+            else if(first > 0 && second > 0 && three >0)
+            {
+                Console.WriteLine("positive");
+            }
+            else if (first==0 || second==0 || three==0)
+            {
+                Console.WriteLine("zero");
+            }
+            
+        }
+    }
+}
