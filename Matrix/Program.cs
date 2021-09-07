421768cba337fb84fcd01446b4ada23b75e21ecb
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-09-07 22:48:12 +0300

    Add files via upload

---
+++ Matrix/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07._NxN_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintMatrix(n);
+
+        }
+
+        static void PrintMatrix(int i)
+        {
+            for (int j = 0; j < i ; j++)
+            {
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write(i+ " ");
+                }
+                Console.WriteLine();
+            }
+            
+            
+
+           
+        }
+    }
+}
