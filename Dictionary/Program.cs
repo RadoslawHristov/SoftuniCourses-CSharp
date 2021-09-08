44b45b9b0d048cda3a08c977400b20d02e9bcc74
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-09-08 12:44:10 +0300

    Add files via upload

---
+++ Dictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Problem222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string username = @"[U$]{1,}([A-Z]{1}[a-z]{4,})[U$]{1,}[P@$]{1,}([a-z]{5,}[0-9]{1,})[P@$]{1,}";
+           
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                MatchCollection match = Regex.Matches(input, username);
+                if (match.Count > 0)
+                {
+                   
+                    count++;
+                    Console.WriteLine("Registration was successful");
+                    foreach (Match item in match)
+                    {
+                        string users = item.Groups[1].Value;
+                        string password = item.Groups[2].Value;
+                        Console.WriteLine($"Username: {users}, Password: {password}");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username or password");
+                }
+            }
+        }
+    }
+}
