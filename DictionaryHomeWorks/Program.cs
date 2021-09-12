9443f142a6565c5dff5e9bc1433af0074d84afb3
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-09-12 22:04:23 +0300

    Add files via upload

---
+++ DictionaryHomeWorks/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, int> resorce = new Dictionary<string, int>();
+            int count = 0;
+
+            while (input != "stop")
+            {
+                int amunt = int.Parse(Console.ReadLine());
+                if (resorce.ContainsKey(input))
+                {
+                    resorce[input] += amunt;
+                }
+                else
+                {
+                    resorce[input] = amunt;
+                }
+                input = Console.ReadLine();
+            }
+
+            foreach (var key in resorce.Keys)
+            {
+                Console.WriteLine($"{key} -> {resorce[key]}");
+            }
+
+        }
+    }
+}
