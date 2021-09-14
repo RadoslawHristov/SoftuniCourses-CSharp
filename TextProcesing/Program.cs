827456f92bf2ee94a756d0b17587989d67937fce
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-09-14 12:39:59 +0300

    Add files via upload

---
+++ TextProcesing/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01._Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var item in input)
+            {
+                if (item.Length < 3 || item.Length > 16)
+                {
+                    continue;
+                }
+
+                bool isvalid = false;
+
+                foreach (var ist in item)
+                {
+                    if (!(char.IsLetter(ist) || char.IsDigit(ist) || ist=='_' || ist=='-'))
+                    {
+                        isvalid = false;
+                        break;
+                    }
+
+                    isvalid = true;
+                }
+
+                if (isvalid)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
