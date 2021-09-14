b52b9277779dfb14e71ccbbe12cf281c04a4665f
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-09-14 12:41:49 +0300

    Add files via upload

---
+++ textProcesingTask/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace _02._Ascii_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstElement = char.Parse(Console.ReadLine());
+            char secontElement = char.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i <input.Length; i++)
+            {
+                if (input[i]==firstElement || input[i]==secontElement)
+                {
+                    continue;
+                }
+                else if (firstElement >=input[i] && secontElement <=input[i]
+                         || secontElement >= input[i] && firstElement <= input[i])
+                {
+                    sum += input[i];
+                }
+                
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
