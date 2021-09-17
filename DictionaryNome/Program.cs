483b9d73ac1ac35ba087c36638e2e48c98fa236d
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-09-17 12:36:35 +0300

    Add files via upload

---
+++ DictionaryNome/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, List<double>> products = new Dictionary<string, List<double>>();
+
+            while (input != "buy")
+            {
+                string[] comamd = input.Split();
+                double price = double.Parse(comamd[1]);
+                double quantity = double.Parse(comamd[2]);
+
+                if (!products.ContainsKey(comamd[0]))
+                {
+                    products.Add(comamd[0], new List<double>() { price, quantity });
+                }
+                else
+                {
+                    double secqua = products[comamd[0]][1];
+                    products[comamd[0]] = new List<double>() { price, secqua + quantity };
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in products)
+            {
+                double total = item.Value[0] * item.Value[1];
+                Console.WriteLine($"{item.Key} -> {total:f2}");
+            }
+        }
+    }
+}
