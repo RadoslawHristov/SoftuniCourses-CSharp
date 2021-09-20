214f74aa2dacfa69f861a29e99188cdcb52002f1
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-09-20 19:48:16 +0300

    Add files via upload

---
+++ HomsWorking/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banned = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            foreach (var item in banned)
+            {
+                text = text.Replace(item, new string('*', item.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
