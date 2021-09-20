9fb55cae02d7487d2c8fa164f21474ca500aed85
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-09-20 19:49:40 +0300

    Add files via upload

---
+++ Arrray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] num = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine($"{Convert.ToDecimal(num[i])} => {Convert.ToDecimal(Math.Round(num[i], MidpointRounding.AwayFromZero))}");
+            }
+        }
+    }
+}
