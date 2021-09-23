735f78c0fef452ec74ae951c89ea1359b6450c39
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-09-23 12:43:31 +0300

    Add files via upload

---
+++ TextprocesingFile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adress = Console.ReadLine();
+
+            int lastIndex = adress.LastIndexOf('\\');
+            string lastelement = adress.Substring(lastIndex+1,adress.Length-1-lastIndex);
+            int indexexeption = lastelement.LastIndexOf('.');
+            string exepelement = lastelement.Substring(indexexeption + 1,lastelement.Length-1-indexexeption);
+            string filenameindex = lastelement.Substring(0, indexexeption);
+           // C:\Internal\training -internal \Template. pptx
+
+            Console.WriteLine($"File name: {filenameindex}");
+            Console.WriteLine($"File extension: {exepelement}");
+        }
+    }
+}
