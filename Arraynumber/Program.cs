ed6a69f6a3c1be199aae91ba653eddf8ea0c200b
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-09-23 12:40:32 +0300

    Add files via upload

---
+++ Arraynumber/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sum = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int bestsquens = 0;
+            int best = 0;
+            for (int i = 0; i < sum.Length; i++)
+            {
+                int curentnum = sum[i];
+                int squens = 1;
+                
+                for (int j =i+1; j < sum.Length; j++)
+                {
+                    int curt = sum[j];
+                    if (curentnum==curt)
+                    {
+                        squens += 1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (squens > bestsquens)
+                {
+                    bestsquens = squens;
+                    best = curentnum;
+                }
+            }
+
+            for (int i = 0; i <bestsquens ; i++)
+            {
+                Console.Write($"{best} ");
+            }
+            
+        }
+    }
+}
