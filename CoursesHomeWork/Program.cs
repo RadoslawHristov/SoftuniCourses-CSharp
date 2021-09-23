8fc64a79bbd61184d28e18b3f87f7281fecf6475
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-09-23 12:47:04 +0300

    Add files via upload

---
+++ CoursesHomeWork/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> averageprice = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!averageprice.ContainsKey(name))
+                {
+                    averageprice.Add(name,new List<double>());
+                    averageprice[name].Add(grade);
+                }
+                else
+                {
+                    averageprice[name].Add(grade);
+                }
+            }
+            foreach (var kvp in averageprice.Where(x => x.Value.Average(x => x) >= 4.50).OrderByDescending(y => y.Value.Average(z => z)))
+            {
+                Console.WriteLine($"{kvp.Key} -> {(kvp.Value.Average(x => x)):f2}");
+            }
+
+        }
+    }
+}
