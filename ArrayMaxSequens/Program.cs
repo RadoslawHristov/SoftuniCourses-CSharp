1b374dbd19cce60f75ae6cbaaa599c6b3952f425
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-09-23 12:38:39 +0300

    Add files via upload

---
+++ ArrayMaxSequens/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            if (array1.Length==1)
+            {
+                Console.WriteLine(array1[0]);
+                return;
+            }
+            int[] condensed = new int[array1.Length-1];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                for (int j = 0; j < condensed.Length-i ;j++)
+                {
+                    condensed[j] = array1[j] + array1[j + 1];
+                }
+                
+                array1 = condensed;
+            }
+
+            Console.WriteLine(array1[0]);
+        }
+    }
+}
