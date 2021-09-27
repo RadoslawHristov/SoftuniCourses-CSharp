ab2a737965f80789bfb159d55fa3dbd130b2a5b1
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-09-27 12:37:42 +0300

    Add files via upload

---
+++ ArrTasks/HomeWork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Print_Numbers_in_Reverse_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Array.Reverse(number);
+            foreach (var VARIABLE in number)
+            {
+                Console.Write(VARIABLE + " ");
+            }
+        }
+    }
+}
