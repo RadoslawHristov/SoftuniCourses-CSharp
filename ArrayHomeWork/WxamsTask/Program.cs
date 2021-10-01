9884bf6189de684fc2eba15bc03f825e1e5fc28b
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-10-01 12:36:01 +0300

    Add files via upload

---
+++ ArrayHomeWork/WxamsTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Destination_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(=|\/)(?<place>[A-Z][a-z]{2,})\1";
+
+            string input = Console.ReadLine();
+            MatchCollection match = Regex.Matches(input, pattern);
+            
+            string result=String.Join(", ",match.Select(x=>x.Groups["place"].Value));
+            string places=String.Join("",match.Select(x=>x.Groups["place"].Value));
+
+            Console.WriteLine($"Destinations: {result}");
+            Console.WriteLine($"Travel Points: {places.Length}");
+        }
+    }
+}
