102f422ade74498934f4a2328a9f8b8918f22126
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-10-06 12:54:36 +0300

    Add files via upload

---
+++ AsosiativeArray/DictyonaryTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Channels;
+
+namespace Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> wordandSin = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string sinonym = Console.ReadLine();
+
+                if (wordandSin.ContainsKey(word)==false)
+                {
+                    wordandSin.Add(word,new List<string>());
+                    wordandSin[word].Add(sinonym);
+                }
+                else
+                {
+                    wordandSin[word].Add(sinonym);
+                }
+            }
+
+            foreach (var item in wordandSin)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ",item.Value)}");
+            }
+        }
+    }
+}
