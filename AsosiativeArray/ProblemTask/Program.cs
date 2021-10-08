135ef24746b1d7fe5c8375499ea526c7fb794d88
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-10-08 12:52:03 +0300

    Add files via upload

---
+++ AsosiativeArray/ProblemTask/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+               
+            Dictionary<char, int> indexes = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] !=' ' && !indexes.ContainsKey(input[i]))
+                {
+                    indexes.Add(input[i],1);
+                    continue;
+                }
+
+                if (input[i] != ' ')
+                {
+                    indexes[input[i]]++;
+                }
+            }
+
+            foreach (var item in indexes)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
