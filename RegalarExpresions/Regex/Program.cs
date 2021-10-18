60ab219e82a12506522bd5a66afaadd1487efb75
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-10-18 12:17:45 +0300

    Add files via upload

---
+++ RegalarExpresions/Regex/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06._Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string pattern = @"(?<=\s)([A-za-z0-9]+[-._]*[A-za-z0-9]+)@([A-Za-z]+(([-.]*)[A-Za-z]+)*\.[a-z]{2,})";
+            MatchCollection maches = Regex.Matches(text, pattern);
+            foreach (Match item in maches)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
